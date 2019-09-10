f3118053a87632c206fcef11fa67bb5daf1094fc
YuxiangLiu <shimotuki.liu@gmail.com>
2019-09-10 20:59:37 +0900

    Some improvements Following change is add. 1. Add check for empty or blank input. 2. Add error msg.

--- app/src/main/java/com/example/hiragarachangetest/MainActivity.java
+++ app/src/main/java/com/example/hiragarachangetest/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.hiragarachangetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,6 +30,27 @@ public class MainActivity extends AppCompatActivity implements IHiragaraChangeVi
 
         final GooLabPresenter presenter = new GooLabPresenter(this);
         final EditText inputBox = findViewById(R.id.inputBox);
+        inputBox.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    InputMethodManager inputMethodManager = (InputMethodManager)textView.getContext()
+                            .getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+                    String sentence = inputBox.getText().toString();
+                    if (TextUtils.isEmpty(sentence.trim())) {
+                        showErrorToast(R.string.output_err_empty_input_msg);
+                    } else {
+                        presenter.getChangeResult(sentence);
+                    }
+                    Log.i(TAG, "input done, sentence change to hiragara is " + sentence);
+                    return true;
+
+                }
+                return false;
+            }
+        });
+
         mHiragaraTextView = findViewById(R.id.outputBox);
         mChangeButton = findViewById(R.id.ChangeButton);
         mChangeButton.setOnClickListener(new View.OnClickListener() {
@@ -30,7 +58,11 @@ public class MainActivity extends AppCompatActivity implements IHiragaraChangeVi
             @Override
             public void onClick(View view) {
                 String sentence = inputBox.getText().toString();
-                presenter.getChangeResult(sentence);
+                if (TextUtils.isEmpty(sentence.trim())) {
+                    showErrorToast(R.string.output_err_empty_input_msg);
+                } else {
+                    presenter.getChangeResult(sentence);
+                }
                 Log.i(TAG, "onClick, sentence change to hiragara is " + sentence);
             }
         });
@@ -57,14 +89,20 @@ public class MainActivity extends AppCompatActivity implements IHiragaraChangeVi
     @Override
     public void onChangeFailure() {
         Log.i(TAG, "Change hiragara error!");
+        showErrorToast(R.string.output_err_general_msg);
+    }
+
+    private void showErrorToast(int errorMsgId) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(MainActivity.this, R.string.output_err_msg,
-                                Toast.LENGTH_LONG).show();
+                        Toast toast = Toast.makeText(MainActivity.this, errorMsgId,
+                                Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
 
                     }
                 });
